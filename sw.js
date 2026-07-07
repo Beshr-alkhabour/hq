792056b1be326bc47ad1a4a7782da03d5a9497ae
Beshr-alkhabour <79600390+Beshr-alkhabour@users.noreply.github.com>
2026-07-07 18:06:51 +0200

    loans tab

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "beshr-hq-v4";
+const CACHE = "beshr-hq-v5";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon-180.png", "./icon-512.png"];
 
 self.addEventListener("install", e => {
